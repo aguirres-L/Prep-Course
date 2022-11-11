6c492a98560daea53955603351f2444b39ace6ff
aguirres.l <aguirres9797@gmail.com>
2022-11-11 16:18:12 -0300

    Segundo Commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -174,7 +174,7 @@ function operadoresLogicos(num1, num2, num3) {
   if(num1 === 0 || num2 === 0 || num3 ===0){
     return("Error")
   }else if (num1 < 0 || num2 < 0 || num3 < 0){
-    return('hay negativos')
+    return('Hay negativos')
   }else if (num1 > num2 && num1> num3){
     return("Número 1 es mayor y positivo")
   }else if (num3 > num1 && num3 > num2){
