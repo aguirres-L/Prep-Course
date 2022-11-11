0bdc0b096e71b4039c25d5447493ef70199bd3df
aguirres.l <aguirres9797@gmail.com>
2022-11-11 11:25:14 -0300

    Commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 4.2;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -97,7 +97,7 @@ function mayorQueCincuenta(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
 
-  if(num < 50){
+  if(num < 51){
     return true
   }else{
    return false
