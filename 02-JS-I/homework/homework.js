625571155eac8977494cbe33e17f968700da7b30
aguirres.l <aguirres9797@gmail.com>
2022-11-11 12:30:02 -0300

    commi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -173,7 +173,7 @@ function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
   
-    return(Math.round(Math.random()*1))
+    return(Math.random())
 }
 
 function esPositivo(numero) {
