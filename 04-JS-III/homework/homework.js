53ca81cd4dc7da96bb39629942d1f12ae6ec7f89
aguirres.l <aguirres9797@gmail.com>
2022-11-12 13:32:10 -0300

    Cuarto Commit Js4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -70,6 +70,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  let result = array.indexOf(elemento)
+  if(result === -1){
+    return false
+  }else{
+    return true
+  }
+  
 }
 
 
