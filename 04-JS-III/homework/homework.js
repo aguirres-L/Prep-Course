8e313a4ca7d7456983d268b25c593ece9fcc2d05
aguirres.l <aguirres9797@gmail.com>
2022-11-13 12:43:55 -0300

    Cuarto Commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -127,15 +127,16 @@ function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 
+  let acumulador = 0
+
   for ( let i = 0; i< arreglo.length; i++){
 
-    if(arreglo[i]>18){
-      acumulador2 = arreglo[i]
-      return(acumulador2) 
-      
+     if(arreglo[i] >18){
+     acumulador = acumulador+1
     }
-
   }
+  
+  return(acumulador)
 
 }
 
